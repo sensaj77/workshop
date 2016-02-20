cf91121d7836087a137da0879b634d939cc8e666
Michal Sawicki <michal@mobilewebpro.pl>
2016-02-20 12:17:36 +0100

    props, state, constructor(), super()

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import TableBody from "./TableBody.jsx";
 import TableHead from "./TableHead.jsx";
 import TodoList from "./TodoList.jsx";
 
+const todos = [ "Task 434123", "Task 434233", "Task 434332" ];
+
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: todos
+    };
+  }
 
   render() {
-  	const todos = [ "Task 1", "Task 2", "Task 3" ];
+  	
     return (
     	<div>
       		<h1>Shop List</h1>
@@ -18,7 +27,7 @@ export default class App extends React.Component {
       		<tfoot>
       		</tfoot>
       		</table>
-      		<TodoList todos={todos}/>
+      		<TodoList todos={this.state.todos}/>
       	</div>
     );
   }
